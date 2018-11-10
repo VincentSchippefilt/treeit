3155e512a924e802ea072fc988dba8fd46252726
Skyp <vincent.schippefilt@gmail.com>
2018-11-10 12:11:59 +0100

    nothing interresting

--- tests/treeittest.py
+++ tests/treeittest.py
@@ -1,21 +1,43 @@
 import unittest
-import treeit
+class Treeit:
+    def tree(self, string):
+        return ""
+
+class Lexer:
+    input = ""
+    def __init__(self, input):
+        self.input = input
+
+    def lex(self):
+        for c in self.input:
+            if (string.isl)
+            
+
+
 
 class Test_StringMsethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    # def test_upper(self):
+    #     self.assertEqual('foo'.upper(), 'FOO')
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+    
+    def test_simple_relation_parent(self):
+        actual = Treeit().tree("a b")
+        expected = """"a
+|
+b
+"""
+        self.assertEqual(actual, expected )
 
 if __name__ == '__main__':
     unittest.main()
--- treeit.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-class test_A(unittest.TestCase):
-    def first(self):
-        self.assertEqual(1,2)
